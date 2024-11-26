3610c7359530d6f3759255dbceeead50af8574ea
brenoborba <brenoborba99@gmail.com>
2024-11-26 11:18:02 +0000

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='min-h-screen flex justify-center items-center'>
+      <h1 className='text-2xl'>React + Vite + TailwindCSS</h1>
+    </div>
+  )
+}
+
+export default App
